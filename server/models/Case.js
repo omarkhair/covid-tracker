179434927df7518409e8cc3abd736316e580c8ca
Omar Khair <omarkhair66@gmail.com>
2022-11-15 23:38:35 +0200

    update the covid case schema

--- server/models/Case.js
+++ server/models/Case.js
@@ -1,21 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const CaseSchema = new mongoose.Schema({
+const CaseSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    latitude:{
-        type: Number,
-        required: true
+    latitude: {
+      type: Number,
+      required: true,
     },
-    longitude:{
-        type: Number,
-        required: true
+    longitude: {
+      type: Number,
+      required: true,
+    },
+    symptoms: {
+      type: String,
+      enum: ["Mild", "Moderate", "Severe"],
+      default: "Moderate",
     },
     temperature: Number,
-});
+  },
+  { timestamps: true }
+);
 
-const Case = mongoose.model('Case', CaseSchema);
+const Case = mongoose.model("Case", CaseSchema);
 
 module.exports = Case;
